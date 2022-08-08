540114bcfabb82df6a2c152825e33d9de04f3d4d
bigshans <26884666+bigshans@users.noreply.github.com>
2022-08-08 16:38:08 +0800

    :tada: init project

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-import '@logseq/libs';
-
-//Inputs 5 numbered blocks when called
-async function insertSomeBlocks (e) {
-  console.log('Open the calendar!')
-  let numberArray = [1, 2, 3, 4, 5]
-  for (const number in numberArray){
-  logseq.App.showMsg("Function has been run")
-  logseq.Editor.insertBlock(e.uuid, `This is block ${numberArray[number]}`, {sibling: true})}
-
+async function autoCommit() {
+  try {
+    await logseq.Git.execCommand(['add', '*']);
+    await logseq.Git.execCommand(['commit', '-c', '"Auto saved by Logseq"']);
+    logseq.App.showMsg('Committed successfully!');
+  } catch (e) {
+    logseq.App.showMsg('Committed failed!');
   }
-  
+}
+/**
+ * entry
+ */
+function main () {
+  logseq.App.registerCommandShortcut({ binding: 'ctrl+s' }, autoCommit);
+}
 
-const main = async () => {
-  console.log('plugin loaded');
-  logseq.Editor.registerSlashCommand('insertBlocks', async (e) => {
-    insertSomeBlocks(e)
-  }
-    
-  )}
-
-logseq.ready(main).catch(console.error);
+// bootstrap
+logseq.ready(main).catch(err => logseq.App.showMsg(err));
